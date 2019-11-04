c7fe977f15499c64127a66dc773671a5641f5fd6
unknown <yukihiro adachi>
2019-11-04 20:07:17 +0900

    code refactoring

--- app/index.js
+++ app/index.js
@@ -1,65 +1,72 @@
 'use strict';
 
-// TODO 定義方法についてES2015を意識した見直し、例えばisLoss(p) {}のような定義方法はあり？
+const _player = {
+    l : 1,
+    r : 1
+};
+const l = "l", r = "r", ll = "ll", lr = "lr", rl = "rl", rr = "rr";
+const _strategyIds = [ ll, lr, rl, rr ];
 module.exports = {
-    player : {
-        l : 1,
-        r : 1
+    strategyIds () {
+        // TODO コピーのキャッシュを持ちたい
+        return Array.from(_strategyIds);
     },
-    strategyIds : [ "ll", "lr", "rl", "rr"],
-    isLoss : function(p) {
+    createPlayer (name) {
+        return Object.assign({name : name}, _player);
+    },
+    isLoss (p) {
         return this.isBreak(this.getL)(p)
             && this.isBreak(this.getR)(p);
     },
-    isNoSide : function(p1, p2) {
+    isNoSide (p1, p2) {
       return this.isLoss(p1) || this.isLoss(p2);
     },
-    getL : function(p) {
+    getL (p) {
         return p.l;
     },
-    getR : function(p) {
+    getR (p) {
         return p.r;
     },
-    getLRById : function(id) {
+    getLRById (id) {
         switch(id) {
-            case "l":
+            case l:
                 return this.getL;
-            case "r":
+            case r:
                 return this.getR;
             default:
                 throw new Error("Illegal Parameter id : " + id)
                 process.exit(1);
         }
     },
-    applyToL : function (f) {
+    applyToL (f) {
         // f関数の戻り値をpToの左手に適用
         return function (pFrom, pTo) {
             pTo.l += f(pFrom);
         };
     },
-    applyToR : function (f) {
+    applyToR (f) {
         // f関数の戻り値をpToの右手に適用
         return function (pFrom, pTo) {
             pTo.r += f(pFrom);
         };
     },
-    getStrategy : function(id) {
+    getStrategy (id) {
         // 攻撃手関数を取得
         switch (id) {
-            case "ll":
+            case ll:
                 return this.applyToL(this.getL);
-            case "lr":
+            case lr:
                 return this.applyToR(this.getL);
-            case "rl":
+            case rl:
                 return this.applyToL(this.getR);
-            case "rr":
+            case rr:
                 return this.applyToR(this.getR);
             default:
                 throw new Error("Illegal Parameter id : " + id)
                 process.exit(1);
         }
     },
-    unUsableStrategy : function (pFrom, pTo, strategyId) {
+    unUsableStrategy (pFrom, pTo, strategyId) {
         // 攻め手の利用不可のケース判定
         //   自身の攻め手が使用不可
         // 　または
@@ -69,10 +76,43 @@ module.exports = {
         return this.isBreak(this.getLRById(from))(pFrom)
             || this.isBreak(this.getLRById(to))(pTo);
     },
-    isBreak : function (f) {
+    isBreak (f) {
         // 対象手がブレイクされているか否かを判定
-        return  function(p) {
+        return function(p) {
             return f(p) % 5 == 0;
         };
+    },
+    doStrategy(pFrom, pTo, nextStrategyId) {
+        // 攻め手実行
+        if (nextStrategyId == null) {
+            throw new Error("over scenario length.");
+        }
+        if (this.unUsableStrategy(pFrom, pTo, nextStrategyId)) {
+            throw new Error("illegal pattern :" + nextStrategyId + ".");
+        }
+        this.getStrategy(nextStrategyId)(pFrom, pTo);
+    },
+    doStrategyAutomatic(pFrom, pTo, nextStrategyId) {
+        // 攻め手のオート実行
+        // 利用不可の攻め手の場合、任意の攻め手を選択する
+        let paramStrategyIdx = _strategyIds.lastIndexOf(nextStrategyId);
+        // 攻め手が決まるまでループ
+        // 攻め手の決定
+        // 係数
+        let idx = 0;
+        while(this.unUsableStrategy(pFrom, pTo, nextStrategyId)) {
+            // 攻め手が見つからない場合、例外を発生させる
+            if (idx == _strategyIds.length) {
+                throw new Error("strategy not found.")
+            }
+            //  攻め手利用不可の場合、攻め手の変更
+            if (idx == paramStrategyIdx) {
+                idx++;
+            }
+            nextStrategyId = _strategyIds[idx];
+            idx++;
+        }
+        // 攻め手を実行
+        this.getStrategy(nextStrategyId)(pFrom, pTo);
     }
 };
--- test/index.test.js
+++ test/index.test.js
@@ -1,44 +1,28 @@
 'use strict';
-// TODO APIで提供するメリットを感じない。軽いロジックなのでサーバではなく画面で処理すればよい。
-// TODO 画面実行のデモ
-//      ターン毎/通知
-//      pA ユーザ１
-//         アクション From:右手？左手？ -> To:右手？左手？　計４パターン
-//         相手の状況によっては一択
-//      pB コンピュータ(auto) | ユーザ２ <- 切り替えは可能か？
-//
-// TODO シナリオ実行
-//      ゲーム毎
-//      動作シナリオを実行して結果を得る
-//
-// TODO プロトタイプの移植
-//      ゲーム毎
-//      現行のプロトタイプ部を移植
-// TODO 上記を踏まえて方針
-//      画面実行デモのためのモジュールを作る(モジュール化の優先度は低）
-//
-const rmg = require('../app/index.js');
+
+// TODO コードの整理整頓
+// TODO テストデータとの突合せも実施
+
+const rmg = require('../app');
 // シナリオ実行
 function entryPointScenario() {
     let scenarios = [];
-    rmg.strategyIds.forEach((crr1) => {
-        rmg.strategyIds.forEach((crr2) => {
-             rmg.strategyIds.forEach((crr3) => {
-                 rmg.strategyIds.forEach((crr4) => {
-                     rmg.strategyIds.forEach((crr5) => {
-                         scenarios.push([crr1, crr2, crr3, crr4, crr5])
-                     });
-                 });
-            });
-        });
-    });
-
-   // console.log(scenarios);
+    for (let s1 of rmg.strategyIds()) {
+        for (let s2 of rmg.strategyIds()) {
+            for (let s3 of rmg.strategyIds()) {
+                for (let s4 of rmg.strategyIds()) {
+                    for (let s5 of rmg.strategyIds()) {
+                        scenarios.push([s1, s2, s3, s4, s5])
+                    }
+                }
+            }
+        }
+    }
 
     // シナリオ実行
     let results = scenarios.reduce((acc, scenario, idx) => {
-        let pA = Object.assign({name : "a"}, rmg.player);
-        let pB = Object.assign({name : "b"}, rmg.player);
+        let pA = rmg.createPlayer("a");
+        let pB = rmg.createPlayer("b");
 
         let turn = 1;
         let result = {idx : idx, scenario : scenario, winner : null, message : null, score : []};
@@ -47,32 +31,24 @@ function entryPointScenario() {
         while(!rmg.isNoSide(pA, pB)) {
             // 先攻
             {
-                let nextStrategyId = scenario[turn * 2 - 2];
-                if (nextStrategyId == null) {
-                    result.message = "over scenario length.";
+                try {
+                    rmg.doStrategy(pA, pB, scenario[turn * 2 - 2]);
+                    result.score.push(getTurnHalf(turn, pA.name, pA, pB));
+                } catch (e) {
+                    result.message = e.message;
                     break;
                 }
-                if(rmg.unUsableStrategy(pA, pB, nextStrategyId)) {
-                    result.message = "illegal pattern :" + nextStrategyId;
-                    break;
-                }
-                rmg.getStrategy(nextStrategyId)(pA, pB);
-                result.score.push(getTurnHalf(turn, pA.name, pA, pB));
             }
 
             // 後攻
             if (!rmg.isLoss(pB)) {
-                let nextStrategyId = scenario[turn * 2 - 1];
-                if (nextStrategyId == null) {
-                    result.message = "over scenario length.";
+                try {
+                    rmg.doStrategy(pB, pA, scenario[turn * 2 - 1]);
+                    result.score.push(getTurnHalf(turn, pB.name, pA, pB));
+                } catch (e) {
+                    result.message = e.message;
                     break;
                 }
-                if(rmg.unUsableStrategy(pB, pA, nextStrategyId)) {
-                    result.message = "illegal pattern :" + nextStrategyId;
-                    break;
-                }
-                rmg.getStrategy(nextStrategyId)(pB, pA);
-                result.score.push(getTurnHalf(turn, pB.name, pA, pB));
             }
             // ターン加算
             turn++;
@@ -90,57 +66,27 @@ function entryPointScenario() {
 
     results.filter((result) => result.winner == "a").forEach(outResult);
     results.filter((result) => result.winner == "b").forEach(outResult);
-//    results.filter((result) => !result.winner).forEach(outResult);
+    results.filter((result) => !result.winner).forEach(outResult);
 };
+
+// スコアを取得
+function getTurnHalf(turn, turnUser, p1, p2) {
+    let per5 = (v) => v % 5;
+    return [turn, turnUser, per5(p1.l), per5(p1.r), per5(p2.l), per5(p2.r)];
+};
+
+// 結果出力
 function outResult(result) {
     console.log(result.idx, result.winner, result.message, result.scenario);
     result.score.forEach((turnHalf) => console.log(turnHalf));
 };
-// 以下、外部処理
-function entryPointProtoType() {
-    let pA = Object.assign({name : "a"}, rmg.player);
-    let pB = Object.assign({name : "b"}, rmg.player);
 
-    let turn = 1;
-
-    // 勝負がつくまでループ
-    while(!rmg.isNoSide(pA, pB)) {
-
-        // 先攻
-        {
-            let nextStrategyId = rmg.strategyIds[(turn) % 4];
-            if(rmg.unUsableStrategy(pA, pB, nextStrategyId)) {
-                console.log("illegal pattern.");
-                break;
-            }
-            rmg.getStrategy(nextStrategyId)(pA, pB);
-            outLog("after turn A", pA, pB);
-        }
-
-        // 後攻
-        if (!rmg.isLoss(pB)) {
-            let nextStrategyId = rmg.strategyIds[(turn) % 4];
-            if(rmg.unUsableStrategy(pB, pA, nextStrategyId)) {
-                console.log("illegal pattern.");
-                break;
-            }
-            rmg.getStrategy(nextStrategyId)(pB, pA);
-            outLog("after turn B", pA, pB);
-        }
-        // ターン加算
-        turn++;
-    }
-
-    if (rmg.isLoss(pB)) {
-        console.log(pA.name + " win!");
-    } else {
-        console.log(pB.name + " win!");
-    }
-};
-
+// オートマティック版
 function entryPointAutomatic() {
-    let pA = Object.assign({name : "a"}, rmg.player);
-    let pB = Object.assign({name : "b"}, rmg.player);
+    let pA = rmg.createPlayer("a");
+    let pB = rmg.createPlayer("b");
+
+    let strategyIds = rmg.strategyIds();
 
     let turn = 1;
 
@@ -148,13 +94,13 @@ function entryPointAutomatic() {
     while(!rmg.isNoSide(pA, pB)) {
         // 先攻
         {
-            doOwnStrategy(pA, pB, turn);
+            rmg.doStrategyAutomatic(pA, pB, strategyIds[turn % 4]);
             outLog("after turn A", pA, pB);
         }
 
         // 後攻
         if (!rmg.isLoss(pB)) {
-            doOwnStrategy(pB, pA, turn);
+            rmg.doStrategyAutomatic(pB, pA, strategyIds[turn % 4]);
             outLog("after turn B", pA, pB);
         }
         // ターン加算
@@ -163,29 +109,14 @@ function entryPointAutomatic() {
 
     if (rmg.isLoss(pB)) {
         console.log(pA.name + " win!");
-    } else {
+    } else if (rmg.isLoss(pA)) {
         console.log(pB.name + " win!");
+    } else {
+        console.log("draw");
     }
 };
 
-function doOwnStrategy(pFrom, pTo, turn) {
-    let nextStrategyId = rmg.strategyIds[(turn) % 4];
-    // 攻め手が決まるまでループ
-    // 攻め手の決定
-    // 係数
-    let coefficient = 0;
-    while(rmg.unUsableStrategy(pFrom, pTo, nextStrategyId)) {
-        //  攻め手利用不可の場合、攻め手の変更
-        coefficient++;
-        nextStrategyId = rmg.strategyIds[(turn + coefficient) % 4];
-    }
-    // 攻め手を実行
-    rmg.getStrategy(nextStrategyId)(pFrom, pTo);
-};
-function getTurnHalf(turn, turnUser, p1, p2) {
-    let per5 = (v) => v % 5;
-    return [turn, turnUser, per5(p1.l), per5(p1.r), per5(p2.l), per5(p2.r)];
-};
+// ログ出力
 function outLog(title, p1, p2) {
     let per5 = (v) => v % 5;
     console.log(title,
@@ -196,6 +127,5 @@ function outLog(title, p1, p2) {
 };
 
 // 実行
-// entryPointProtoType();
-// entryPointAutomatic();
-entryPointScenario();
+// entryPointScenario();
+entryPointAutomatic();
